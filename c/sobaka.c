aee1da058159b5c222cbdff98f70b49c084f5707
Rudenko <ruok@MacBook-Pro-Rudenko.local>
2019-03-31 12:33:45 +0300

    У попа была собака

---
+++ c/sobaka.c
@@ -0,0 +1,11 @@
+// подключаем библиотеку для использования printf
+#include "stdio.h"
+
+char sobaka_print(){
+    printf("у попа была собака, он её любил \n она съела кусок мяса, он её убил \n в землю закопал \n и надпись написал, что ");
+    printf(sobaka_print());
+}
+
+int main(){
+sobaka_print();
+}
